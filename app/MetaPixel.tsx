4d10c632aa8a2e98d61a7984fe3dc5584785c401
Sonam Tshering <tsheringsonam36@gmail.com>
2026-07-19 14:07:09 +1000

    Fix meta pixel

--- app/MetaPixel.tsx
+++ app/MetaPixel.tsx
@@ -1,27 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
 import Script from "next/script";
 
 export default function MetaPixel() {
-  useEffect(() => {
-    if (typeof window !== "undefined" && (window as any).fbq) {
-      (window as any).fbq("track", "PageView");
-    }
-  }, []);
-
   return (
     <>
-      <Script
-        id="facebook-pixel"
-        strategy="afterInteractive"
-        onLoad={() => {
-          if ((window as any).fbq) {
-            (window as any).fbq("init", "2083372059196540");
-            (window as any).fbq("track", "PageView");
-          }
-        }}
-      >
+      <Script id="facebook-pixel" strategy="afterInteractive">
         {`
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -38,8 +22,21 @@ export default function MetaPixel() {
           s.parentNode.insertBefore(t,s)}
           (window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '2083372059196540');
+          fbq('track', 'PageView');
         `}
       </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=2083372059196540&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
     </>
   );
 }

8c7b108981cd875798cae9244b8290d756add965
Sonam Tshering <tsheringsonam36@gmail.com>
2026-07-19 13:02:59 +1000

    added meta pixer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
@@ -15,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Dragon Vista Tours | Discover Bhutan",
-  description: "Authentic Bhutan cultural, luxury, trekking and festival tours.",
+  description:
+    "Authentic Bhutan cultural, luxury, trekking and festival tours.",
 };
 
 export default function RootLayout({
@@ -28,10 +30,46 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2083372059196540');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-full flex flex-col">
-        <Navbar/>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2083372059196540&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        <Navbar />
+
         {children}
-        </body>
+      </body>
     </html>
   );
 }
